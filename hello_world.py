7eaa49c3479ffa4c6268d010f99e86aa35270121
achahal9 <arjun.chahal@esmt.berlin>
2025-12-10 11:01:18 +0100

    docs: message update for branch

--- hello_world.py
+++ hello_world.py
@@ -1,4 +1,5 @@
 print("Welcome to MAAI Workshop")
 print("Please write your name here\n")
 # Each teammate should enter thier name individually and push the update to GitHub
-print("Arjun")
+print("This is the team space for Arjun")
+print("Date: Dec 10, 2025")
